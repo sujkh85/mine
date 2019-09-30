9b5898840d73fe43a98d8399a05801b03677ecf9
sujkh <sujkh@velic.io>
2019-09-30 12:16:48 +0900

    리소스 작업

--- docs/precache-manifest.22887bd2848746b6ea7b99e201003e11.js
+++ docs/precache-manifest.22887bd2848746b6ea7b99e201003e11.js
@@ -1,38 +1,38 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "32295345269ffb0a0ba7d558e17c8f2c",
-    "url": "/index.html"
+    revision: "32295345269ffb0a0ba7d558e17c8f2c",
+    url: "/index.html"
   },
   {
-    "revision": "3b74ad5c8710f36f941a",
-    "url": "/static/css/main.0a270fac.chunk.css"
+    revision: "3b74ad5c8710f36f941a",
+    url: "./static/css/main.0a270fac.chunk.css"
   },
   {
-    "revision": "62c76caf8e37076e41ec",
-    "url": "/static/js/2.c2f45c01.chunk.js"
+    revision: "62c76caf8e37076e41ec",
+    url: "./static/js/2.c2f45c01.chunk.js"
   },
   {
-    "revision": "3b74ad5c8710f36f941a",
-    "url": "/static/js/main.0749d07c.chunk.js"
+    revision: "3b74ad5c8710f36f941a",
+    url: "./static/js/main.0749d07c.chunk.js"
   },
   {
-    "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    revision: "42ac5946195a7306e2a5",
+    url: "./static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "78099a3b3ff3548e9684cd55bae666a6",
-    "url": "/static/media/bomb.78099a3b.png"
+    revision: "78099a3b3ff3548e9684cd55bae666a6",
+    url: "./static/media/bomb.78099a3b.png"
   },
   {
-    "revision": "1f042630a552a96f8145869af4877703",
-    "url": "/static/media/boom.1f042630.mp3"
+    revision: "1f042630a552a96f8145869af4877703",
+    url: "./static/media/boom.1f042630.mp3"
   },
   {
-    "revision": "8026a5397d4583ce083a654d8294a92f",
-    "url": "/static/media/click.8026a539.mp3"
+    revision: "8026a5397d4583ce083a654d8294a92f",
+    url: "./static/media/click.8026a539.mp3"
   },
   {
-    "revision": "4f86f5e9718d81d242af45afa065cfaf",
-    "url": "/static/media/victory.4f86f5e9.m4a"
+    revision: "4f86f5e9718d81d242af45afa065cfaf",
+    url: "./static/media/victory.4f86f5e9.m4a"
   }
 ]);
